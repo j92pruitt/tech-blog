a077e0444ff1d7050524217f2d02c76752d6fc8c
j92pruitt <j92pruitt@gmail.com>
2021-08-10 18:23:30 -0700

    Added functionality to serve a basic template homepage

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/homeRoutes')
 
 // Initialize express app and set up port.
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Initialize Handlebars engine and set app's view engine to handlebars.
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // Middleware for parsing requests
 app.use( express.json() );
 app.use( express.urlencoded( {extended: true} ) );
+
+app.use(routes);
 
 app.listen( PORT, () => console.log(`Listening on port: ${PORT}`) );
