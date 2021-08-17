84451cf92051b9674aabf5e70221c7d301b63fba
j92pruitt <j92pruitt@gmail.com>
2021-08-16 17:37:16 -0700

    Added helper to format dates in handlebars.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
+const helpers = require('./utils/helpers')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -28,7 +29,7 @@ async function init() {
     app.use( session(sess) );
 
     // Initialize Handlebars engine and set app's view engine to handlebars.
-    const hbs = exphbs.create();
+    const hbs = exphbs.create({ helpers });
     app.engine('handlebars', hbs.engine);
     app.set('view engine', 'handlebars');
 
